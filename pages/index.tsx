fd733158f2b2552a53ca28a3cf1d1a59c8ac0c66
FerdiVilqin <gimabun@gmail.com>
2025-05-26 14:22:38 +0700

    ✅ Add working index.tsx for landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,5 @@
-import UploadButton from '../components/UploadButton'
-
 export default function Home() {
-  return (
-    <main>
-      <h1>DarkGPT UI Final</h1>
-      <UploadButton />
-    </main>
-  )
+  return <h1>DarkGPT UI Final</h1>
 }
+
 
